d5cd5ad38c5bb4cbba1a1f0ec6b3ee07edbb88d3
brennv <brennv@users.noreply.github.com>
2016-09-28 08:33:18 -0700

    clean up

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-# from redis import Redis
-
 from .views import api, load_config
 
 
 def create_app(debug=False):
-    # redis = Redis(host="redis", db=0)
     app = Flask(__name__)
     app.debug = debug
     app.register_blueprint(api)
